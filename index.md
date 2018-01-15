d188fe7cfe19036e9ec006ebf47dc7583cd0dd96
udareaniket <udareaniket@gmail.com>
2018-01-15 08:10:04 -0500

    Insta + cleanup
    Added Instagram feed on about me
    some cleanup.

--- index.md
+++
@@ -1,5 +0,0 @@
-## Welcome to My Page, where I post links to my demos of my repos.
-
-GitHub User Score: [Demo](https://udareaniket.github.io/gitUserScore/){: .btn}
-
-GitHub User Search: [Demo](https://udareaniket.github.io/gitUserSearch/){: .btn}
--- js/application.js
+++ js/application.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var app = angular.module('myApp', [ 'ui.router', 'ngRoute', 'ngMaterial',
-		'ngMessages' ]);
+var app = angular.module('myApp', [ 'ui.router', 'ngRoute']);
 app.config(function($stateProvider) {
 	$stateProvider.state('about', {
 		url : '/about',
@@ -18,9 +17,27 @@ app.config(function($stateProvider) {
 	}).state('timeline', {
 		url : '/timeline',
 		templateUrl : 'pages/timeline.html'
-	})
-	.state('otherwise', {
+	}).state('otherwise', {
 		url : '*path',
 		templateUrl : 'pages/about.html'
 	})
+});
+app.controller('aboutController', function($window, $state, $route, $http,
+		$rootScope, $scope, $location, $filter) {
+	var name = "aniketudare", items;
+	$http.get("https://www.instagram.com/" + name + "/?__a=1").success(
+			function(data) {
+				items = data.user.media.nodes;
+				$rootScope.imageList = items;
+				console.log(items)
+				setTimeout(function() {
+					$('.slider').slider();
+					$('.carousel').carousel({
+						indicators:'true',
+						padding:50,
+						noWrap:'true',
+					});
+				}, 1);
+
+			});
 });
