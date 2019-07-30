e9ecc2d8721677de166d7cdf2a09fd6bc85bf817
Aniket Udare <udareaniket@gmail.com>
2019-07-30 06:43:29 -0400

    Added resume Preview

--- js/application.js
+++ js/application.js
@@ -3,7 +3,9 @@ var myApp = angular.module('myApp', []);
 $(function() {
 	$('[data-toggle="tooltip"]').tooltip()
 });
-
+$(document).ready(function(){
+    $('#resumeIframe').attr('src', 'https://drive.google.com/file/d/1zvRKL_e0ZzoLsADXpdOB_rXkSAq4WaP9/preview');    
+});
 myApp.controller('controller', [ '$scope', '$http', function($scope, $http) {
 	$scope.projectList = null;
 	$http.get('js/projects.json').then(function(response) {
