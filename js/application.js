82d82f99755bdf4339d52995420d0d0743b45ab8
Aniket Udare <udareaniket@gmail.com>
2019-08-06 02:39:52 -0400

    Analytics and scores
    Added back analytics
    Updated GTMetrix scores

--- js/application.js
+++ js/application.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
     $('#resumeIframe').attr('src', 'https://drive.google.com/file/d/1zvRKL_e0ZzoLsADXpdOB_rXkSAq4WaP9/preview');
     $('#resumeIframe').attr('width', $("#quickDiv").width()*0.75);
     $('#resumeIframe').attr('height', $("#quickDiv").width()*0.75);
+    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+	ga('create', 'UA-114286060-1', 'auto');
+	ga('send', 'pageview');
 });
 myApp.controller('controller', [ '$scope', '$http', function($scope, $http) {
 	$scope.projectList = null;
